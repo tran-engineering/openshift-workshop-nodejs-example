c02f2678b6afa5118b989f126fb2fba928dcd1c9
Khôi Tran <tran@puzzle.ch>
2018-11-20 17:18:39 +0100

    updated server.js

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-var http = require('http')
-
+const http = require('http')
 http.createServer(function (req, res) {
+  console.log(new Date(), req.method, req.url)
+
   res.writeHead(200, {'Content-Type': 'text/plain'})
-  res.end("hello")
+
+  res.end(process.env.HELLO_MSG || 
+    'Hello! Please try to set the environment variable: HELLO_MSG!')
 }).listen(8080)
 
-console.log("Server listening on 8080")
+console.log('Server listening on :8080...')
